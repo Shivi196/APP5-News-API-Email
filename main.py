b10276c168d2c54dac94dd3dbb53121ee8956268
misha <misha07.sharma@gmail.com>
2025-01-29 13:58:05 +0530

    Day 27 : Send and emails with title and description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from send_email import send_email
 api_key = "98e9182496d84e178a13259aa9035e4a"
 # url = "https://finance.yahoo.com/?guccounter=1"
 url = "https://newsapi.org/v2/everything?q=tesla&\
@@ -11,8 +11,34 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
+body = ""
+
+
 # Access the article titles and description
 for articles in content["articles"]:
-    print(articles["title"])
-    print(articles["description"])
+
+    # title = articles["title"]
+    # description = articles["description"]
+
+#     print(description)
+#     if title and description is not None:
+#         body = f"""\
+# Subject :  News Email from the API
+#
+# {title} + "\n"
+# Description of news : {description} + 2 * "\n"
+# """
+
+
+    if articles["title"] and articles["description"] is not None:
+
+        body = body + articles["title"] + "\n" + articles["description"] + 2 * "\n"
+
+body = body.encode("utf-8")
+send_email(body)
+
+
 # print(type(content))
+
+
+
---
+++ send_email.py
@@ -0,0 +1,29 @@
+import smtplib,ssl
+import os
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context()
+    sender_email = "sharmabruno310@gmail.com"
+    sender_password = os.getenv("PASSWORD")
+    user_email = "sharmabruno310@gmail.com"
+
+#     message = '''\
+# Subject : Test Email
+# In the context of secure communication, an SSL Context is a collection of settings and configurations that define the parameters for establishing a secure connection using SSL/TLS protocols.
+# '''
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, user_email, message)
+
+# For Setting up environment variable below are steps
+# 1. Open terminal
+# 2. type in touch ~/.zshrc; open ~/.zshrc
+# 3. Write in zshrc file - export PASSWORD="ahbw faam tral qbgo"
+# 4. Restart Pycharm and check again in console by typing import os nd then os.getenv("PASSWORD") it should show the value for it
+# 5. for more info for this or windows os check Day23 last video of Ardit
+
+
+
+
